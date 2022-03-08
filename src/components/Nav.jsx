676796fb9c4f8180f6ac405a6119333d7e6973a7
tubez7 <thescopeboys@hotmail.com>
2022-03-08 11:56:42 +0000

    add further CSS styling to improve consistency on various displays

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,27 +6,27 @@ export default function Nav() {
   const { loggedInUser } = useContext(UserContext);
   return (
     <nav className="Nav_Bar">
-      <div className="User_Icon">
-        {loggedInUser.username}
-        <img
-          src={loggedInUser.avatar_url}
-          className="Avatar_Image"
-          alt="Profile for the logged-in user"
+      <div className="Nav_Bar_Items">
+        <div className="User_Icon">
+          <img
+            src={loggedInUser.avatar_url}
+            className="Avatar_Image"
+            alt="Profile for the logged-in user"
           />
+          <p>{loggedInUser.username}</p>
+        </div>
+        <Link to="/" className="Nav_Link">
+          <button>Home</button>
+        </Link>
+        <Link to="/topics" className="Nav_Link">
+          <button>Topics</button>
+        </Link>
+        <Link to="/user" className="Nav_Link">
+          <button>User Profiles</button>
+        </Link>
       </div>
-      <Link to="/" className="Nav_Link">
-      <button>Home</button>
-      </Link>
-      <Link to="/topics" className="Nav_Link">
-        <button>Topics</button>
-      </Link>
-      <Link to="/user" className="Nav_Link">
-      <button>User Profiles</button>
-      </Link>
-      
     </nav>
   );
 }
-          
 
 //conditional logic with user profile? IF LOGGED IN USERNAME IS NOT THE DEFAULT LINK TO USERPROFILE PAGE NOT THE LOGIN PAGE.
