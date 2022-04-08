974cae5a620beca4efa72fffd3a8192c85879795
tubez7 <thescopeboys@hotmail.com>
2022-04-08 23:07:05 +0100

    clean redundant code

--- src/components/DeleteCom.jsx
+++ src/components/DeleteCom.jsx
@@ -1,41 +1,41 @@
-import { useState } from "react";
 import { deleteComment } from "../utils/api";
 
 export default function DeleteCom({ commentId, setComments, setComNum }) {
-  const [error, setError] = useState(null);
 
   let comIndexToDelete = 0;
 
   let deletedCom = {};
 
   const handleClick = () => {
-    setComments((currentComments) => {
-      comIndexToDelete = currentComments.findIndex(
-        (comment) => comment.comment_id === commentId
-      );
-      deletedCom = currentComments[comIndexToDelete];
-      setComNum((currentComNum) => currentComNum - 1);
-
-      return currentComments.filter(
-        (comment) => comment.comment_id !== commentId
-      );
-    });
-    deleteComment(commentId).catch((err) => {
-      setError(err); // will use when adding login functionality
-      setComNum((currentComNum) => currentComNum + 1);
-      alert("Unable to delete comment");
+    if (window.confirm("test")) {
       setComments((currentComments) => {
-        const comClone = [...currentComments];
-        comClone.splice(comIndexToDelete, 0, deletedCom);
-        return comClone;
+        comIndexToDelete = currentComments.findIndex(
+          (comment) => comment.comment_id === commentId
+        );
+        deletedCom = currentComments[comIndexToDelete];
+        setComNum((currentComNum) => currentComNum - 1);
+
+        return currentComments.filter(
+          (comment) => comment.comment_id !== commentId
+        );
       });
-    });
+      deleteComment(commentId).catch(() => {
+        setComNum((currentComNum) => currentComNum + 1);
+        alert("Unable to delete comment");
+        setComments((currentComments) => {
+          const comClone = [...currentComments];
+          comClone.splice(comIndexToDelete, 0, deletedCom);
+          return comClone;
+        });
+      });
+    } else {
+      return (
+        <div>{commentId && <button onClick={handleClick}>Delete</button>}</div>
+      );
+    }
   };
 
   return (
-    <div>
-      {commentId && <button onClick={handleClick}>Delete</button>}
-    </div>
+    <div>{commentId && <button onClick={handleClick}>Delete</button>}</div>
   );
-};
-      
+}
