2e18a12ed1386e0003e3a4cbac4f7db569aafc64
tubez7 <thescopeboys@hotmail.com>
2022-04-20 18:34:09 +0100

    refactor error handling

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getArticleById } from "../utils/api";
-import SingleArticle from "./SingleArticle";
-import { Link } from "react-router-dom";
 
-export default function Article({loggedIn}) {
+import SingleArticle from "./SingleArticle";
+import ErrorPage from "./ErrorPage";
+
+export default function Article({ loggedIn }) {
   const [isLoading, setIsLoading] = useState(true);
   const { article_id: articleId } = useParams();
   const [article, setArticle] = useState({});
@@ -23,32 +24,10 @@ export default function Article({loggedIn}) {
       });
   }, [articleId]);
 
-  if (isLoading) return <h4 className="user-msg">PLEASE WAIT. ARTICLE LOADING....</h4>;
+  if (isLoading)
+    return <h4 className="user-msg">PLEASE WAIT. ARTICLE LOADING....</h4>;
 
-  if (error === 400)
-    return (
-      <>
-        <h4 className="user-msg">HMMM...I'M AFRAID YOU'VE MADE A BAD REQUEST PAL!</h4>
-        <Link className="user-msg" to="/">Return to Homepage</Link>
-      </>
-    );
-
-  if (error === 404)
-    return (
-      <>
-        <h4 className="user-msg">TERRIBLY SORRY!! WE COULD NOT FIND THAT ARTICLE!!</h4>
-        <Link className="user-msg" to="/">Return to Homepage</Link>
-      </>
-    );
-
-  if (error)
-    return (
-      <>
-        <h4 className="user-msg">OH DEAR! SOMETHING'S GONE WRONG!</h4>
-        <Link className="user-msg" to="/">Return to Homepage</Link>
-      </>
-    );
+  if (error) return <ErrorPage error={error} />;
 
   return <SingleArticle {...article} loggedIn={loggedIn} />;
 }
-
